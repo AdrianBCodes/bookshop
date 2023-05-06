8c185ec018f36d2977122873e8d3d458e37fca5d
AdrianBCodes <adrian.brudzynski1@o2.pl>
2023-05-06 12:28:40 +0200

    Removed endpoints/methods that will not be used in new application version.

--- bookshop-java/src/main/java/org/bookstore/Main.java
+++ bookshop-java/src/main/java/org/bookstore/Main.java
@@ -1,28 +1,11 @@
 package org.bookstore;
 
-import org.bookstore.book.Book;
-import org.bookstore.book.BookCategory;
-import org.bookstore.book.BookService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication()
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    // For presentation
-    @Bean
-    CommandLineRunner run(BookService bookService){
-        return args -> {
-          bookService.addBook(new Book("book1", "1st book for example", BookCategory.HORROR, BigDecimal.ONE ));
-          bookService.addBook(new Book("book2", "2nd book for example", BookCategory.DRAMA, BigDecimal.TEN ));
-          bookService.addBook(new Book("book3", "3rd book for example", BookCategory.THRILLER, BigDecimal.ZERO ));
-        };
     }
 }
--- bookshop-java/src/main/java/org/bookstore/book/BookRepository.java
+++ bookshop-java/src/main/java/org/bookstore/book/BookRepository.java
@@ -3,16 +3,10 @@ package org.bookstore.book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository {
     Optional<Book> findBookById(Long id);
     Page<Book> findAllBooks(Pageable page);
-    Page<Book> findAllFreeBooks(Pageable pageable);
-    Page<Book> findAllPaidBooks(Pageable pageable);
-    Page<Book> findAllByName(String name, Pageable pageable);
-    Page<Book> findAllWithCategory(BookCategory category, Pageable pageable);
     Long saveBook(Book entity);
-    void deleteBook(Book entity);
 }
