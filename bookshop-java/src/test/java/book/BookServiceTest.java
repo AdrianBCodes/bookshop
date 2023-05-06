98e0661ce5aece7b6e6439686ab7bc1aedc9b55a
AdrianBCodes <adrian.brudzynski1@o2.pl>
2023-05-06 12:18:55 +0200

    Removed unnecessary tests. Added Mockito to used tests.

--- bookshop-java/src/test/java/book/BookServiceTest.java
+++ bookshop-java/src/test/java/book/BookServiceTest.java
@@ -2,152 +2,74 @@ package book;
 
 import org.bookstore.book.Book;
 import org.bookstore.book.BookCategory;
+import org.bookstore.book.BookRepository;
 import org.bookstore.book.BookService;
+import org.bookstore.exceptions.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
-import static org.assertj.core.api.BDDAssertions.*;
+import static org.assertj.core.api.BDDAssertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.catchThrowable;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 
 class BookServiceTest {
+    private BookRepository bookRepositoryMock;
+
     private BookService bookService;
 
     @BeforeEach
-    void init(){
-        bookService = new BookService(new FakeBookRepository());
-    }
-
-    @Test
-    public void addBook() {
-        // given
-        Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        //when
-        bookService.addBook(book);
-        // then
-        assertThat(bookService.findBookById(1L)).isEqualTo(book);
-    }
-
-    @Test
-    public void editBook() {
-        // given
-        Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        Book book2 = new Book(1L,"Book2", "Desc2", BookCategory.DRAMA, BigDecimal.ONE);
-        bookService.addBook(book);
-        //when
-        bookService.editBook(book2);
-        // then
-        assertThat(bookService.findBookById(1L)).isEqualTo(book2);
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        bookRepositoryMock = mock(BookRepository.class);
+        bookService = new BookService(bookRepositoryMock);
     }
 
     @Test
     public void findById() {
         // given
         Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        bookService.addBook(book);
+        given(bookRepositoryMock.saveBook(book)).willReturn(1L);
+        given(bookRepositoryMock.findBookById(1L)).willReturn(Optional.of(book));
+        bookRepositoryMock.saveBook(book);
         // when
         var result = bookService.findBookById(1L);
         // then
+        then(bookRepositoryMock).should().findBookById(1L);
         assertThat(result).isEqualTo(book);
     }
 
     @Test
     public void findById_ThrowsException() {
         // given
-
+        given(bookRepositoryMock.findBookById(1L)).willReturn(Optional.empty());
         // when
-        var exception = catchThrowable(() -> bookService.findBookById(1L));
+        var exception = catchThrowable( () -> bookService.findBookById(1L));
         // then
-        assertThat(exception).isInstanceOf(RuntimeException.class);
+        assertThat(exception).isInstanceOf(NotFoundException.class);
     }
 
     @Test
     public void findAll() {
         // given
         Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        Book book2 = new Book(2L,"Book2", "Desc2", BookCategory.THRILLER, BigDecimal.ZERO);
-        bookService.addBook(book);
-        bookService.addBook(book2);
+        Book book2 = new Book(2L,"Book2", "Desc2", BookCategory.DRAMA, BigDecimal.ONE);
+        given(bookRepositoryMock.findAllBooks(PageRequest.of(0, 5)))
+                .willReturn(new PageImpl<>(List.of(book, book2), PageRequest.of(0, 5), 2));
         // when
-        var result = bookService.findAllBooks(PageRequest.of(10,10));
+        var result = bookService.findAllBooks(PageRequest.of(0,5));
         // then
+        then(bookRepositoryMock).should().findAllBooks(PageRequest.of(0,5));
         assertThat(result.getContent().size()).isEqualTo(2);
         assertThat(result.getContent().get(0)).isEqualTo(book);
         assertThat(result.getContent().get(1)).isEqualTo(book2);
-    }
-
-    @Test
-    public void findAllFreeBooks() {
-        // given
-        Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        Book book2 = new Book(2L,"Book2", "Desc2", BookCategory.THRILLER, BigDecimal.ZERO);
-        bookService.addBook(book);
-        bookService.addBook(book2);
-        // when
-        var result = bookService.findAllFreeBooks(PageRequest.of(10,10));
-        // then
-        assertThat(result.getContent().size()).isEqualTo(1);
-        assertThat(result.getContent().get(0)).isEqualTo(book2);
-    }
-
-    @Test
-    public void findAllPaidBooks() {
-        // given
-        Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        Book book2 = new Book(2L,"Book2", "Desc2", BookCategory.THRILLER, BigDecimal.ZERO);
-        bookService.addBook(book);
-        bookService.addBook(book2);
-        // when
-        var result = bookService.findAllPaidBooks(PageRequest.of(10,10));
-        // then
-        assertThat(result.getContent().size()).isEqualTo(1);
-        assertThat(result.getContent().get(0)).isEqualTo(book);
-    }
-
-    @Test
-    public void findAllByName() {
-        // given
-        Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        Book book2 = new Book(2L,"Book2", "Desc2", BookCategory.THRILLER, BigDecimal.ZERO);
-        Book book3 = new Book(3L,"Something", "Desc2", BookCategory.THRILLER, BigDecimal.ZERO);
-        bookService.addBook(book);
-        bookService.addBook(book2);
-        bookService.addBook(book3);
-        // when
-        var result = bookService.findAllByName("Book", PageRequest.of(10,10));
-        // then
-        assertThat(result.getContent().size()).isEqualTo(2);
-        assertThat(result.getContent().get(0).getName()).isEqualTo("Book1");
-        assertThat(result.getContent().get(1).getName()).isEqualTo("Book2");
-    }
-
-    @Test
-    public void findAllByCategory() {
-        // given
-        Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        Book book2 = new Book(2L,"Book2", "Desc2", BookCategory.THRILLER, BigDecimal.ZERO);
-        bookService.addBook(book);
-        bookService.addBook(book2);
-        // when
-        var result = bookService.findAllByCategory(BookCategory.DRAMA, PageRequest.of(10,10));
-        // then
-        assertThat(result.getContent().size()).isEqualTo(1);
-        assertThat(result.getContent().get(0)).isEqualTo(book);
-
-    }
-    @Test
-    public void deleteBook() {
-        // given
-        Book book = new Book(1L,"Book1", "Desc1", BookCategory.DRAMA, BigDecimal.ONE);
-        bookService.addBook(book);
-        // when
-        var foundBook = bookService.findBookById(1L);
-        bookService.deleteBook(book);
-        var exception = catchThrowable(() -> bookService.findBookById(1L));
-        // then
-        assertThat(foundBook).isEqualTo(book);
-        assertThat(exception).isInstanceOf(RuntimeException.class);
     }
 }
