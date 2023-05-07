7d46ecf7017ed296fdcb1fec7ca7a012124ff289
AdrianBCodes <adrian.brudzynski1@o2.pl>
2023-05-07 23:39:04 +0200

    Added Swagger.

---
+++ bookshop-java/src/main/java/org/bookstore/configuration/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package org.bookstore.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@ComponentScan(basePackages = "org.bookstore.configuration")
+public class SwaggerConfig {
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("org.bookstore.controller"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Bookshop API")
+            .description("API designed for Bookshop")
+            .version("1.0")
+            .build();
+    }
+}
--- bookshop-java/src/main/java/org/bookstore/controller/BookController.java
+++ bookshop-java/src/main/java/org/bookstore/controller/BookController.java
@@ -1,5 +1,7 @@
 package org.bookstore.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.bookstore.book.Book;
 import org.bookstore.book.BookService;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/books")
+@Api(value = "Books API")
 public class BookController {
     private final BookService bookService;
 
@@ -18,11 +21,13 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Find a book by Id", response = Book.class)
     ResponseEntity<Book> findBookById(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.findBookById(id));
     }
 
     @GetMapping
+    @ApiOperation(value = "Find all books paged", response = Page.class)
     ResponseEntity<Page<Book>> findAllBooks(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "3") int size
